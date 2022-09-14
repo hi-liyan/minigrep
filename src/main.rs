ff94ddf188e89b1571807ec8c9e8b836de768ee1
李炎 <18525589998@163.com>
2022-09-14 20:10:18 +0800

    文档注释

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::{env, process};
 use minigrep::{self, Config};
 
+/// 跟着 Bilibili 杨旭老师的《Rust 编程语言入门教程》敲的第一个小实例
+///
+/// # Example
+/// ###
+/// ```bash
+/// cargo run abc hello.txt
+/// ```
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("参数解析错误：{}", err);
